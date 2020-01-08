073b7ed404a1140df63629aa62e2cf79161bcdf8
Hoyoung Jung <honux77@gmail.com>
2020-01-08 18:23:47 +0900

    Init spring boot project
    - spring boot 2.2.2 초기화
    - web, devtools 의존성 추가
    - start.spring.io

---
+++ src/main/java/net/honux/qna2020/Qna2020Application.java
@@ -0,0 +1,13 @@
+package net.honux.qna2020;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Qna2020Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Qna2020Application.class, args);
+	}
+
+}
---
+++ src/test/java/net/honux/qna2020/Qna2020ApplicationTests.java
@@ -0,0 +1,13 @@
+package net.honux.qna2020;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Qna2020ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
