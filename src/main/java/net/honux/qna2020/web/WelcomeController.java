f0fd0a944d4ffee14b4844d7910e88029bfcef47
Hoyoung Jung <honux77@gmail.com>
2020-01-11 17:40:10 +0900

    Implement simple dynamic web page
    - 콘트롤러에 인자 및 모델 추가
    - 템플릿에 해당 인자를 처리하도록 {{name}}, {{age}} 추가

--- src/main/java/net/honux/qna2020/web/WelcomeController.java
+++ src/main/java/net/honux/qna2020/web/WelcomeController.java
@@ -1,13 +1,17 @@
 package net.honux.qna2020.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WelcomeController {
 
     @GetMapping("/hello")
-    public String welcome() {
+    public String welcome(String name, int age, Model model) {
+        System.out.println(name);
+        model.addAttribute("name", name);
+        model.addAttribute("age", age);
         return "welcome";
     }
 }
