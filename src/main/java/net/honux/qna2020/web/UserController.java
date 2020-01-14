805479c5d024df4e9f912727a273bbdc5878f52f
Hoyoung Jung <honux77@gmail.com>
2020-01-14 16:16:06 +0900

    Update UserController to handle POST
    회원가입 폼 처리를 위해 콘트롤러에 인자 추가
    템플릿에 처리 코드 추가 및 정상동작 확인

--- src/main/java/net/honux/qna2020/web/UserController.java
+++ src/main/java/net/honux/qna2020/web/UserController.java
@@ -1,13 +1,19 @@
 package net.honux.qna2020.web;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
 
-    @GetMapping("/create")
-    public String create(){
+    @PostMapping("/create")
+    public String create(String email, String name, String password1, String password2, Model model) {
+        System.out.printf("%s %s %s %s\n", email, name, password1, password2);
+        model.addAttribute("email", email);
+        model.addAttribute("name", name);
+        model.addAttribute("password1", password1);
+        model.addAttribute("password2", password2);
         return "create";
     }
 }
