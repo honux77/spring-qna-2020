a01c9d4058cae1fceefb637f2d4f5d84742d81f7
Hoyoung Jung <honux77@gmail.com>
2020-01-14 17:43:06 +0900

    Implement list users
    - 회원가입시 리스트에 추가
    - 전체 사용자 목록 보여주도록 리다이렉트
    - GET /list 구현

--- src/main/java/net/honux/qna2020/web/UserController.java
+++ src/main/java/net/honux/qna2020/web/UserController.java
@@ -2,15 +2,28 @@ package net.honux.qna2020.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class UserController {
+
+    private List <User> users = new ArrayList<>();
 
     @PostMapping("/create")
     public String create(User user, Model model) {
         System.out.println(user);
         model.addAttribute("user", user);
-        return "create";
+        users.add(user);
+        return "redirect:/list";
+    }
+
+    @GetMapping("/list")
+    public String list(Model model) {
+        model.addAttribute("users", users);
+        return "list";
     }
 }
