ddd767e5e1ba3e4a5fb64cc913b4b9b70e88dad2
Hoyoung Jung <honux77@gmail.com>
2020-01-25 00:12:31 +0900

    [step3-2] Implement User save, list using JPA
    User 객체가 실제 디비에 저장되고 불러올 수 있도록 구현함
    스텝 3-2 마무리

--- src/main/java/net/honux/qna2020/web/UserController.java
+++ src/main/java/net/honux/qna2020/web/UserController.java
@@ -1,5 +1,6 @@
 package net.honux.qna2020.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,18 +13,20 @@ import java.util.List;
 public class UserController {
 
     private List <User> users = new ArrayList<>();
+    @Autowired
+    private UserRepository userRepository;
 
     @PostMapping("/create")
     public String create(User user, Model model) {
         System.out.println(user);
         model.addAttribute("user", user);
-        users.add(user);
+        userRepository.save(user);
         return "redirect:/list";
     }
 
     @GetMapping("/list")
     public String list(Model model) {
-        model.addAttribute("users", users);
+        model.addAttribute("users", userRepository.findAll());
         return "list";
     }
 }
