16de3a17dcfa768dc7f76651f091a528d53b9569
Hoyoung Jung <honux77@gmail.com>
2020-02-05 08:08:48 +0900

    사용자 관련 URL /users/ 로 정리
    RequestMapping 어노테이션 활용
    템플릿 폴더도 함께 정리
    
    - /users/ 로 사용자 관련 기능 url 변경
    - 템플릿 경로도 /users/ 아래로 변경
    - 사용자 콘트롤러 클래스에 @RequestMapping("users") 어노테이션 사용
    - 사용자 추가 URL: POST /users/new
    - 사용자 조회 URL:  GET /users/

--- src/main/java/net/honux/qna2020/web/UserController.java
+++ src/main/java/net/honux/qna2020/web/UserController.java
@@ -5,27 +5,29 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
+@RequestMapping("/users/")
 public class UserController {
 
     @Autowired
     private UserRepository userRepository;
 
-    @PostMapping("/create")
+    @PostMapping("/new")
     public String create(User user, Model model) {
         //System.out.println(user);
         //model.addAttribute("user", user);
         userRepository.save(user);
-        return "redirect:/list";
+        return "redirect:/users/";
     }
 
-    @GetMapping("/list")
+    @GetMapping("/")
     public String list(Model model) {
         model.addAttribute("users", userRepository.findAll());
-        return "list";
+        return "/users/list";
     }
 }
