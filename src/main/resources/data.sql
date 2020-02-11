2a1a677b3fa0600338c4626e391e2c2ab9eea640
Hoyoung Jung <honux77@gmail.com>
2020-02-11 12:49:48 +0900

    초기 데이터를 넣기 위해 data.sql 추가
    편리하군.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into user (email, name, password, ID) values ('honux77@gamil.com', '정호영', '1234', 101);
+insert into user (email, name, password, ID) values ('honux78@gamil.com', '정호일', '1235', 102);
+
