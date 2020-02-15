3ed41f183a240577014a804b015bd0fb7b9e9513
Hoyoung Jung <honux77@gmail.com>
2020-02-15 11:43:13 +0900

    질문 목록 보여주기 기능 구현
    - 날짜 포맷해서 리턴
    - 사용자 및 질문 카운트 구현

--- src/main/java/net/honux/qna2020/web/Question.java
+++ src/main/java/net/honux/qna2020/web/Question.java
@@ -1,9 +1,13 @@
 package net.honux.qna2020.web;
 
+import org.hibernate.annotations.CreationTimestamp;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Entity
 public class Question {
@@ -16,6 +20,9 @@ public class Question {
     private String writer;
     private String contents;
 
+    @CreationTimestamp
+    private LocalDateTime dateCreated;
+
     public void setTitle(String title) {
         this.title = title;
     }
@@ -26,5 +33,29 @@ public class Question {
 
     public void setContents(String contents) {
         this.contents = contents;
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public LocalDateTime getDateCreated() {
+        return dateCreated;
+    }
+
+    public String getFormattedDate() {
+        if (dateCreated == null) {
+            return "";
+        }
+        return dateCreated.format(DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm:ss"));
     }
 }
--- src/main/java/net/honux/qna2020/web/WelcomeController.java
+++ src/main/java/net/honux/qna2020/web/WelcomeController.java
@@ -1,6 +1,8 @@
 package net.honux.qna2020.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpSession;
@@ -8,8 +10,17 @@ import javax.servlet.http.HttpSession;
 @Controller
 public class WelcomeController {
 
+    @Autowired
+    private QuestionRepository questionRepository;
+    @Autowired
+    private UserRepository userRepository;
+
     @GetMapping("/")
-    public String welcome() {
+    public String welcome(Model model) {
+        model.addAttribute("questions", questionRepository.findAll());
+        model.addAttribute("num_users", userRepository.count());
+        model.addAttribute("num_questions", questionRepository.count());
         return "welcome";
+
     }
 }
