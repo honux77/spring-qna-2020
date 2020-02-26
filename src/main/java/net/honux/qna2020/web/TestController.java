5b7796dd378140e9cfe56301882055118d74b3de
Hoyoung Jung <honux77@gmail.com>
2020-02-26 16:14:42 +0900

    테스트 스프링 포워드 URL 테스트함
    - 포워드 시에서 서블릿 리퀘스트 값은 유지되지만 model은 유지되지 않음
    - 어디에 쓸지는 잘 모르겠다.

--- src/main/java/net/honux/qna2020/web/TestController.java
+++ src/main/java/net/honux/qna2020/web/TestController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.server.ResponseStatusException;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 @RequestMapping("/test")
 public class TestController {
@@ -19,6 +21,21 @@ public class TestController {
         }
         model.addAttribute("id", id);
         return "/test/404";
+    }
+
+    @GetMapping("/forward/{id}")
+    public String forwardTest(@PathVariable Long id,  Model model) {
+        model.addAttribute("v1", "v1");
+        System.out.println("forwardTest1");
+        return "forward:/test/forward2";
+    }
+
+    @GetMapping("/forward2")
+    public String  forwardTest2(HttpServletRequest req, Model model) {
+        System.out.println("forwardTest2");
+        System.out.println(req.getRequestURL());
+        model.addAttribute("v2", "v2");
+        return "/test/forward";
     }
 
 }
